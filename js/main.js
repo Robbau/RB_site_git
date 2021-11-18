be6ad5a1e773fcc8ca96e67fd214e12b3f490788
Robin Baudisch <rbaudisch.hcie-m2019@fh-salzburg.ac.at>
2021-11-18 10:57:29 +0100

    state update before refactoring

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
+
+// Look for .hamburger
+let hamburger = document.querySelector(".hamburger");
+var hamburgerContent = document.querySelector("#header");
+var navigation = document.querySelector(".navigation");
+
+
+
+  // On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+  hamburgerContent.classList.toggle("active");
+  navigation.classList.toggle("activeNav");
+  });
+
+
 var coll = document.getElementsByClassName("projectSlider");
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("sliderActive");
+    this.classList.toggle("active");
+    // this.classList.toggle("sliderActive");
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
@@ -13,17 +32,4 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
-// Look for .hamburger
-var hamburger = document.querySelector(".hamburger");
-var hamburgerContent = document.querySelector("#header");
-var navigation = document.querySelector(".navigation");
-// On click
-hamburger.addEventListener("click", function() {
-  // Toggle class "is-active"
-  hamburger.classList.toggle("is-active");
-  // Do something else, like open/close menu
-  hamburgerContent.classList.toggle("active");
-  navigation.classList.toggle("activeNav");
 
-
-});
--- js/nav.js
+++ js/nav.js
@@ -1,36 +1,56 @@
-var coll = document.getElementsByClassName("projectSlider");
-var i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
+// Look for .hamburger
+let hamburger = document.querySelector(".hamburger");
+var hamburgerContent = document.querySelector("#header");
+var navigation = document.querySelector(".navigation");
+
+
+
+  // On click
+hamburger.addEventListener("click", function() {
+  // Toggle class "is-active"
+  hamburger.classList.toggle("is-active");
+  // Do something else, like open/close menu
+  hamburgerContent.classList.toggle("active");
+  navigation.classList.toggle("activeNav");
   });
-}
 
 
-(function() {
 
-	let hamburger = {
-		nav: document.querySelector('#nav'),
-		navToggle: document.querySelector('.nav-toggle'),
 
-		initialize() {
-			this.navToggle.addEventListener('click',
-        () => { this.toggle(); });
-		},
+// var coll = document.getElementsByClassName("projectSlider");
+// var i;
 
-		toggle() {
-			this.navToggle.classList.toggle('expanded');
-			this.nav.classList.toggle('expanded');
-		},
-	};
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function() {
+//     this.classList.toggle("active");
+//     var content = this.nextElementSibling;
+//     if (content.style.display === "block") {
+//       content.style.display = "none";
+//     } else {
+//       content.style.display = "block";
+//     }
+//   });
+// }
 
-	hamburger.initialize();
 
-}());
+// (function() {
+
+// 	let hamburger = {
+// 		nav: document.querySelector('#nav'),
+// 		navToggle: document.querySelector('.nav-toggle'),
+
+// 		initialize() {
+// 			this.navToggle.addEventListener('click',
+//         () => { this.toggle(); });
+// 		},
+
+// 		toggle() {
+// 			this.navToggle.classList.toggle('expanded');
+// 			this.nav.classList.toggle('expanded');
+// 		},
+// 	};
+
+// 	hamburger.initialize();
+
+// }());
